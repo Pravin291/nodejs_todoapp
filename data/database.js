004a4e33f8856f182d16ddb75984a9506cb0a7c5
Pravin Bhosale <pb6884845@gmail.com>
2023-08-10 14:09:48 +0530

    Fixed Engine

--- data/database.js
+++ data/database.js
@@ -5,6 +5,6 @@ export const connectDB = ()=>{
 
     mongoose.connect(process.env.MONGO_URI,{
         dbName:"backend",
-    }).then(()=>console.log("db connect"))
+    }).then((c)=>console.log(`Database connectd with ${c.connection.host}`))
       .catch((e) => console.log(e))
 }
